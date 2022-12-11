ffc73129cc36da55d76ebfefe8131f419d5b86c4
Callum B <10206838+calmelb@users.noreply.github.com>
2022-12-11 13:03:31 +0100

    Basic string splitting implemented

--- src/01.py
+++ src/01.py
@@ -1,6 +1,52 @@
 from utils.api import get_input
-
+import re
 input_str = get_input(1)
 
+
+
+
 # WRITE YOUR SOLUTION HERE
+
+'''
+This solves day 1 challenge for the advent of code 2022
+
+The challenge involves calories, with below being an example list: 
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+
+
+- The first Elf is carrying food with 1000, 2000, and 3000 Calories, a total of 6000 Calories.
+- The second Elf is carrying one food item with 4000 Calories.
+- The third Elf is carrying food with 5000 and 6000 Calories, a total of 11000 Calories.
+- The fourth Elf is carrying food with 7000, 8000, and 9000 Calories, a total of 24000 Calories.
+- The fifth Elf is carrying one food item with 10000 Calories.
+
+
+Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+
+# '''
+# print(input_str)
+# print(repr(input_str))
+sections = input_str.split('\n\n')
+print(sections)
+
+split_string = []
+for sub in sections:
+    split_string.append(sub.split('\n'))
+
+    
+print(split_string)
+
 
